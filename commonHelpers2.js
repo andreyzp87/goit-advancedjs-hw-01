611354ac580ba1c086c5ba8cbddf52859ecdaab1
andreyzp87 <andreyzp87@gmail.com>
2024-06-01 09:03:35 +0000

    Deploying to gh-pages from @ andreyzp87/goit-advancedjs-hw-01@32e633502d0517c93ec3b0a0a8a7cc57db19e08d 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{P as a,t as i}from"./assets/vendor-1e54b975.js";const t="videoplayer-current-time",o=new a("vimeo-player"),e=localStorage.getItem(t);e&&o.setCurrentTime(e);const m=function(r){localStorage.setItem(t,r.seconds)};o.on("timeupdate",i(m,1e3));
+//# sourceMappingURL=commonHelpers2.js.map
